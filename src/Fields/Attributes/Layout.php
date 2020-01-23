c8710332b270dcf36db119ef9d2641c98dcabeff
Vincent Klaiber <vincent@doubledip.se>
2020-01-23 21:28:57 +0100

    Update copyright

--- src/Fields/Attributes/Layout.php
+++ src/Fields/Attributes/Layout.php
@@ -1,12 +1,12 @@
 <?php
 
-/*
- * This file is part of WordPlate.
- *
- * (c) Vincent Klaiber <hello@doubledip.se>
+/**
+ * Copyright (c) Vincent Klaiber.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * @see https://github.com/wordplate/acf
  */
 
 declare(strict_types=1);
