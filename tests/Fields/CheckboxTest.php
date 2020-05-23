9801e3a60cf95038831b175444e1a61ac6f89a5d
jbbouchet <jb@studiomagnetique.fr>
2020-05-23 17:00:29 +0200

    Added layout tests

--- tests/Fields/CheckboxTest.php
+++ tests/Fields/CheckboxTest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace WordPlate\Tests\Acf\Fields;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use WordPlate\Acf\Fields\Checkbox;
 
@@ -22,5 +23,19 @@ class CheckboxTest extends TestCase
     {
         $field = Checkbox::make('Checkbox')->toArray();
         $this->assertSame('checkbox', $field['type']);
+    }
+
+    public function testLayout()
+    {
+        $field = Checkbox::make('Checkbox with layout')->layout('horizontal')->toArray();
+        $this->assertSame('horizontal', $field['layout']);
+    }
+
+    public function testLayoutException()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid argument layout [invalid_layout].');
+
+        Checkbox::make('Checkbox')->layout('invalid_layout');
     }
 }
