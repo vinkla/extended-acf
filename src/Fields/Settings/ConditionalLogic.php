f14c982b1bd62e0d744de72c79030d1480788a51
Olivier Gorzalka <olivier@amphibee.fr>
2023-03-31 14:24:48 +0200

    Fix phpunit test

--- src/Fields/Settings/ConditionalLogic.php
+++ src/Fields/Settings/ConditionalLogic.php
@@ -17,7 +17,7 @@ trait ConditionalLogic
 {
     public function conditionalLogic(array $rules): static
     {
-        $this->settings['conditional_logic'] = $rules;
+        $this->settings['conditional_logic'] = array_merge($this->settings['conditional_logic'] ?? [], $rules);
         return $this;
     }
 }
