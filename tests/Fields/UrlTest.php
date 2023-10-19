4f2e7b17dd27a153df8ef7053070eaf53f9fbca1
Vincent Klaiber <vincent@doubledip.se>
2023-10-19 12:57:51 +0200

    Rename `Url` field to `URL`

--- tests/Fields/UrlTest.php
+++ tests/Fields/UrlTest.php
@@ -13,23 +13,20 @@ declare(strict_types=1);
 
 namespace Extended\ACF\Tests\Fields;
 
-use Extended\ACF\Fields\Url;
+use Extended\ACF\Fields\URL;
 use PHPUnit\Framework\TestCase;
 
-class UrlTest extends TestCase
+class URLTest extends TestCase
 {
     public function testType()
     {
-        $field = Url::make('Url')->get();
+        $field = URL::make('URL')->get();
         $this->assertSame('url', $field['type']);
     }
 
     public function testDefault()
     {
-        $field = Url::make('Default')->default('hotpink')->get();
+        $field = URL::make('Default')->default('hotpink')->get();
         $this->assertSame('hotpink', $field['default_value']);
-
-        $field = Url::make('Default Value')->default('dodgerblue')->get();
-        $this->assertSame('dodgerblue', $field['default_value']);
     }
 }
